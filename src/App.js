1a0ded70694609adbb5e3c215e4d34e6a5dade74
Michael <ninjacksnake@hotmail.com>
2023-07-06 08:27:18 -0400

    Adding icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import Home from "./pages/Home";
+
 
 function App() {
   return (
